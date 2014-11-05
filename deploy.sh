84c2cd2a8f0160b3465a69c33285019c9b80fd74
sunzl <sunzl>
2014-11-05 13:56:11 +0800

    modify description text

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
  
 # main config
-PLUGINSLUG="wp-favorite-posts"
+PLUGINSLUG="wp-favorite-posts-extended"
 CURRENTDIR=`pwd`
 MAINFILE="wp-favorite-posts.php" # this should be the name of your main php file in the wordpress plugin
  
--- wp-favorite-posts.php
+++ wp-favorite-posts.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: WP Favorite Posts
-Plugin URI: http://nxsn.com/my-projects/wp-favorite-posts-plugin/
-Description: Allows users to add favorite posts. This plugin use cookies for saving data so unregistered users can favorite a post. Put <code>&lt;?php wpfp_link(); ?&gt;</code> where ever you want on a single post. Then create a page which includes that text : <code>[wp-favorite-posts]</code> That's it!
+Plugin Name: WP Favorite Posts Extended
+Plugin URI: https://github.com/avrilmaomao/wp-favorite-posts-extended
+Description:
 Version: 1.6.2
 Author: Huseyin Berberoglu
 Author URI: http://nxsn.com
